d0c06d0a4812e90d1f46803a46373dc708568a74
TranTrungNghia98 <136548687+TranTrungNghia98@users.noreply.github.com>
2023-07-31 10:09:55 +0700

    title scene
    - Add background image.
    - Add Text & Button.
    - Add Start & Quit Game features.

--- Assets/Scripts/UI/StartUIHandle.cs
+++ Assets/Scripts/UI/StartUIHandle.cs
@@ -6,27 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class StartUIHandle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Main Scene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
 #if UNITY_EDITOR
             EditorApplication.ExitPlaymode();
 #else
     Application.Quit();
 #endif
-        }
-
-        else if (Input.anyKey)
-        {
-            SceneManager.LoadScene("Main Scene");
         }
     }
 }
